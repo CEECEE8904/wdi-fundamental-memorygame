553c798927142866356627ba8f84b128f3e3eb88
CEECEE8904 <50652256+CEECEE8904@users.noreply.github.com>
2019-05-20 00:44:50 -0400

    Add files via upload
    5/20/2019 Latest changes

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-/*16 & 17 here*/
-cardsInPlay.push('queen');
-console.log("User flipped queen");
 
-/*Card two flip*/
-var cardTwo = cards[2];
-cardsInPlay.push('king');
-console.log("User flipped king");
- if (cardsInPlay.length === 2) {
- if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match!"); 
- else alert("Sorry, try again"); }
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) { 
+        alert('You found a match!');
+    } else {
+        alert('Sorry, try again.');
+    }   
+}
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[0]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
 
- function flipCard (cardId){
- var cardOne = cards[0];
- var cardTwo = cards[2];
- }
+flipCard(0);
+flipCard(2);
