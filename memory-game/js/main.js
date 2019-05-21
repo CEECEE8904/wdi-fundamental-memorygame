a560388c1790e6d0927eeda7c6c3d2f0eb1f0251
CEECEE8904 <50652256+CEECEE8904@users.noreply.github.com>
2019-05-20 23:03:29 -0400

    Add files via upload
    Changes Made 5/20/2019 11pm

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,15 +1,42 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-    if (cardsInPlay[0] === cardsInPlay[1]) { 
-        alert('You found a match!');
+    this['src'] = (cards[0,1,2,3].cardImage);
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
     } else {
-        alert('Sorry, try again.');
-    }   
-}
+    alert("Sorry, try again.");
+    console.log(cardsInPlay);
+    cardsInPlay[0].setAttribute('src', 'images/back.png');
+    cardsInPlay[1].setAttribute('src', 'images/back.png');
+  }
+};
+
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[0]);
   if (cardsInPlay.length === 2) {
     checkForMatch();
